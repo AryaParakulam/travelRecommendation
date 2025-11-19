825849f21571936f17512714b2e637f6e10d8f8f
Arya <aryababu1357@gmail.com>
2025-11-18 20:24:10 -0500

    travelReco

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -6,12 +6,36 @@ function showDestinations(){
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-        if (data.countries.cities.description.toLowerCase().includes(keyword))
-            recommendationDiv.innerHTML=data.countries.name
-        endif
+        if ('beaches'.includes(keyword)){
+            //recommendationDiv.innerHTML=data.beaches[0].name;
+            viewResult(data.beaches)
+        }
+
+        //if (data.includes(keyword))
+        //    recommendationDiv.innerHTML=data.keys()
+        //endif
     })
     .catch(error => {
 
     });
 }
-document.getElementById('btnSearch').addEventListener('click',showDestinations)
+document.getElementById('btnSearch').addEventListener('click',showDestinations);
+
+function viewResult(destinations){
+     destinations.forEach(element => {
+        var destDiv = document.createElement('div');
+        destDiv.classList.add('destinations');
+  
+        var name = document.createElement('h2');
+        name.textContent = element.name;
+
+        var description = document.createElement('p');
+        description.textContent = element.description;
+
+        
+
+        destDiv.appendChild(name);
+        destDiv.appendChild(description);
+        recommendationDiv.appendChild(destDiv);
+     });
+}
